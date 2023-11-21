28969171f713793a65499f8c99f5157c3e3f3423
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-21 17:37:49 +0100

    mb del

---
+++ 2.JavaScript/1.Introducrion/script.js
@@ -0,0 +1,27 @@
+/*
+*/
+
+// let Title = document.title
+
+// let newTitle = document.title = ('Modifying the DOM');
+// console.log(newTitle);
+
+// let pink = document.body.style.backgroundColor = '#FF69B4'
+
+// function random_bg_color() {
+//   let x = Math.floor(Math.random() * 256); // Math random 0 > 255
+//   let y = Math.floor(Math.random() * 256); // Math random 0 > 255
+//   let z = Math.floor(Math.random() * 256); // Math random 0 > 255
+//   let bgColor = "rgb(" + x + "," + y + "," + z + ")"; // Prend les chiffre rando et en créer un rgb aleatoire
+// console.log(bgColor);
+
+//   document.body.style.background = bgColor;
+//   }
+
+// random_bg_color();
+
+// let child = document.body.children;
+
+// for (child of child){
+//   console.log(child);
+// }
