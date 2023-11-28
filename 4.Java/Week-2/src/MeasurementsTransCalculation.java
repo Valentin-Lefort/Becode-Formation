4fab9082e972555c26464739dd531e3595ac0d4c
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-28 17:03:42 +0100

    Challenge1

--- 4.Java/Week-2/src/MeasurementsTransCalculation.java
+++ 4.Java/Week-2/src/MeasurementsTransCalculation.java
@@ -4,12 +4,11 @@ import javax.swing.*;
 public class MeasurementsTransCalculation {
   public static void main(String[] args){
     
-    JOptionPane modal = new JOptionPane();
     JList<String> li = new JList<>(new String[]{ "METER", "CENTIMETER", "MILLIMETER" });
     
-    modal.showMessageDialog(null, li, "Select something", JOptionPane.PLAIN_MESSAGE);
-    String askLength = modal.showInputDialog("Give us a length :");
-    String askWidth = modal.showInputDialog("Give us a width");
+    JOptionPane.showMessageDialog(null, li, "Select something", JOptionPane.PLAIN_MESSAGE);
+    String askLength = JOptionPane.showInputDialog("Give us a length :");
+    String askWidth = JOptionPane.showInputDialog("Give us a width");
     
     String metric = li.getSelectedValue();
     
@@ -21,21 +20,19 @@ public class MeasurementsTransCalculation {
         Double mtcLength = length * 100;
         Double mtcWidth = width * 100;
         double result = mtcLength * mtcWidth;
-        System.out.println(result);
-        modal.showMessageDialog(null, result);
-        System.out.println(result);
+        JOptionPane.showMessageDialog(null, result);
       } else if (metric.equals(calc.CENTIMETER.name())) {
         double result = length * width;
-        modal.showMessageDialog(null,result);
+        JOptionPane.showMessageDialog(null,result);
       } else if (metric.equals(calc.MILLIMETER.name())) {
         Double milliTcLength = length / 10;
         Double mtmWidth = width / 10;
         Double result = milliTcLength * mtmWidth;
-        modal.showMessageDialog(null,(result + "cm²"));
+        JOptionPane.showMessageDialog(null,(result + "cm²"));
       } else { System.out.print("Error"); }
     } catch (NumberFormatException e) {
       String error = "Error";
-      modal.showMessageDialog(null, error);
+      JOptionPane.showMessageDialog(null, error);
       
     }
     
