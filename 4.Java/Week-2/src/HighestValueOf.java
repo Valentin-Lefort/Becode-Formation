ca1e89d3b8f766ad4fb58d5b8b93f510a19577f4
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-30 09:06:25 +0100

    Challenge 6 Week 2

--- 4.Java/Week-2/src/HighestValueOf.java
+++ 4.Java/Week-2/src/HighestValueOf.java
@@ -1,8 +1,26 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class HighestValueOf {
   
   public static void main(String[] args){
-  
-  
+    
+    String fileName = "src/effects-of-covid19-on-trade.csv";
+    
+    try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+      String values = lines.map(line -> Arrays.asList(line.split(",")))
+              .filter(y -> y.contains("2019") && y.contains("China") && Objects.equals(y.get(5), "All") && Objects.equals(y.get(6), "All"))
+              .skip(1)
+              .max(Comparator.comparing(y -> Long.parseLong(y.get(y.size() - 2)))).orElseThrow(Exception::new).toString();
+      System.out.println(values);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    
   }
   
 }
