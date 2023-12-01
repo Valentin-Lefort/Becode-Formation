4da469dcaf7d1cb7315089431b1605df138eec8e
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-12-01 09:36:25 +0100

    Challenge 8 Week 2

---
+++ 4.Java/Week-2/src/Stacks.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Stacks
+{
+  public static void main(String[] args)
+  {
+    Stack<String> stk = new Stack<>();
+    
+    if(stk.isEmpty()){
+      
+      stk.push("Justine");
+      stk.push("Virginie");
+      stk.push("Kimi");
+      stk.push("Valentin");
+      
+      System.out.println("Stack : " + stk);
+      
+      String java = stk.peek();
+      System.out.println("Top Element : " + java);
+      
+    } else {
+      stk.pop();
+      System.out.println(stk);
+    }
+    
+
+    
+  }
+}
