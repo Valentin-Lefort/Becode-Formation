7f6f41cd08731aee7a2f99c06a3579dd14392472
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-12-14 16:38:26 +0100

    add challenge 4

---
+++ 4.Java/Week-4/Challenge-4/src/test/java/org/example/challenge4/Challenge4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.challenge4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge4ApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
